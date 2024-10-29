395a04d52acff131dc8f7eb768576449d70b1be5
Anomaly-Games <contact@fermi-paradox.com>
2024-10-29 07:05:19 +0100

    Added Changelog

--- Assets/_Complete-Game/Scripts/BoardManager.cs
+++ Assets/_Complete-Game/Scripts/BoardManager.cs
@@ -15,8 +15,7 @@ namespace Completed
 		{
 			public int minimum; 			//Minimum value for our Count class.
 			public int maximum; 			//Maximum value for our Count class.
-			
-			
+			 
 			//Assignment constructor.
 			public Count (int min, int max)
 			{
